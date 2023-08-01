1ef803e398f3c089af53f164fdbf775c6e5e7d67
Ahmet Çitak <acitak06@hotmail.com>
2023-08-01 07:07:17 +0300

    Addded Hardware Design

--- LoRa_Basic_Test/LoRaSender/LoRaSender.ino
+++ LoRa_Basic_Test/LoRaSender/LoRaSender.ino
@@ -1,31 +1,45 @@
-#include <SPI.h>
-#include <LoRa.h>
+#include "deneyap.h" //Deneyap Geliştirme Kartı kütüphanesinin programa 
 
-int counter = 0;
+#include <ESP32Servo.h> //Servo motor kütüphanesinin programa eklenmesi
 
-void setup() {
-  Serial.begin(115200);
-  while (!Serial);
+#define sag_led D4 //servo motorun bağlandığı D0 pininin tanımlaması
+#define sol_led D8
+#define L1_pin D14 //sağ ışık sensörünün bağlandığı D14 pininin tanımlaması
+#define L2_pin D15 //sol ışık sensörünün bağlandığı D15 pininin tanımlaması
 
-  Serial.println("LoRa Sender");
+Servo myservo1; //servo motora ilişkin değişken tanımlaması
 
-  if (!LoRa.begin(868E6)) {
-    Serial.println("Starting LoRa failed!");
-    while (1);
-  }
+int sensor1, sensor2; //LDR’lerin değişkenlerinin tanımlanması
+int pos = 0; //servo motor pozisyonuna ilişkin değişken tanımlaması
+
+void setup() 
+{
+  pinMode(sol_led, OUTPUT);
+  pinMode(sag_led, OUTPUT);
+  myservo.attach(SERVOPIN);
+
 }
 
 void loop() {
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+  sensor1 = digitalRead(L1_pin); //sağ ışık sensöründen gelen dijital verinin okunması
+ sensor2 = digitalRead(L2_pin); //sol ışık sensöründen gelen dijital verinin okunması
 
-  // send packet
-  LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
-
-  counter++;
-
-  delay(5000);
+ if (sensor1>sensor2) //sağ LDR’ye düşen ışık > sol LDR’ye düşen ışık
+ {
+ digitalWrite(sag_led,1);
+ digitalWrite(sol_led,0);
+ }
+ else if(sensor2>sensor1) //sol LDR’ye düşen ışık > sağ LDR’ye düşen ışık
+ {
+ digitalWrite(sag_led,0);
+ digitalWrite(sol_led,1);
+ }
+ else if(sensor2=sensor1) //sağ LDR’ye düşen ışık = sol LDR’ye düşen ışık
+ {
+ digitalWrite(sag_led,1);
+ digitalWrite(sol_led,1);
+ }
+delay(5);
 }
+
+
